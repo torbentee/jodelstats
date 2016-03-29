7cd2420a6659d9fe328b6faae4a2f858cd907191
ppati000 <ppati000@me.com>
2016-03-29 04:37:34 +0200

    Add 301 redirect for heroku domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :redirect_to if Rails.env.production?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  private
+
+    def redirect_to
+      domain_to_redirect_to = 'www.jodelstats.com'
+      domain_exceptions = ['jodelstats.com', 'www.jodelstats.com']
+      should_redirect = !(domain_exceptions.include? request.host)
+      new_url = "#{request.protocol}#{domain_to_redirect_to}#{request.fullpath}"
+      redirect_to new_url, status: :moved_permanently if should_redirect
+    end
+
 end
