24549fbea1734e36a70f2cdbf663c67282e737b8
ppati000 <ppati000@me.com>
2016-07-22 00:41:37 +0200

    Change update_cities preference

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,7 +2,7 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.singleton
 
-if ENV["jodelstats_update"]
+if ENV["jodelstats_update"] == 'enabled'
   puts "JODEL UPDATE ON"
   scheduler.every("5m") do
     JodelCityController.update_cities
