827aadc3f2b58826e4f4049e70a663eb0ac5bb2e
ppati000 <ppati000@me.com>
2016-07-22 00:37:30 +0200

    Log message for update_cities

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -3,7 +3,10 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
 if ENV["jodelstats_update"]
+  puts "JODEL UPDATE ON"
   scheduler.every("5m") do
     JodelCityController.update_cities
   end
+else
+  puts "JODEL UPDATE OFF"
 end
