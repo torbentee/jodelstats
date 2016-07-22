a5aed757a02e4d83ef5fc9d69fe8627ea60acea4
ppati000 <ppati000@me.com>
2016-07-22 09:22:04 +0200

    Make update interval dynamic

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,7 +4,7 @@ scheduler = Rufus::Scheduler.singleton
 
 if ENV["jodelstats_update"] == 'enabled'
   puts "JODEL UPDATE ON"
-  scheduler.every("5m") do
+  scheduler.every(ENV["jodelstats_refresh_interval"] || "5m") do
     JodelCityController.update_cities
   end
 else
